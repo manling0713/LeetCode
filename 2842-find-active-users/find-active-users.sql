1328437fd5f6e0fb8336afa1419e1e21040851a6
Queenie Chao <manling000713@gmail.com>
2024-03-21 13:05:32 -0700

    Time: 2070 ms (22.86%) | Memory: 0B (100.00%) - LeetSync

--- 2842-find-active-users/find-active-users.sql
+++ 2842-find-active-users/find-active-users.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
-WITH row_user AS (
-    SELECT *, ROW_NUMBER() OVER() AS rn FROM Users
+WITH cte AS (
+    SELECT *, ROW_NUMBER() OVER() AS rn 
+    FROM Users
 )
 
-SELECT DISTINCT(a.user_id) FROM row_user a, row_user b WHERE a.user_id = b.user_id AND a.rn <> b.rn AND ABS(DATEDIFF(b.created_at, a.created_at)) <= 7
+SELECT DISTINCT(a.user_id)
+FROM cte a, cte b
+WHERE a.user_id = b.user_id AND a.rn <> b.rn
+    AND ABS(DATEDIFF(b.created_at,a.created_at)) <= 7
