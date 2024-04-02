f58aaf6363a6a53541cb83095927acc86d2af2e5
Queenie Chao <manling000713@gmail.com>
2024-04-02 00:36:45 -0700

    Time: 537 ms (90.18%) | Memory: 0B (100.00%) - LeetSync

--- 3400-find-bursty-behavior/find-bursty-behavior.sql
+++ 3400-find-bursty-behavior/find-bursty-behavior.sql
@@ -1,20 +1,40 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
-
-
-WITH A AS(
-    SELECT * FROM Posts WHERE '2024-02-01' <= post_date AND post_date <= '2024-02-28'
+-- create cte for avg weekly posts
+WITH AVG_WEEKLY AS (
+    SELECT user_id,
+           COUNT(*) / 4 AS avg_weekly_posts
+    FROM Posts
+    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
+    GROUP BY user_id
 ),
-B AS(
-    SELECT user_id, COUNT(*)/4 AS avg_weekly_posts  FROM A GROUP BY 1
+-- create cte for agg daily posts
+AGG_DAILY AS (
+    SELECT user_id,
+           post_date,
+           COUNT(*) AS daily_posts
+    FROM Posts
+    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
+    GROUP BY user_id, post_date
 ),
-C AS(
-    SELECT user_id, post_date, COUNT(*) OVER(PARTITION BY user_id ORDER BY post_date RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW ) AS cnt7 FROM A
+-- create cte to get agg posts in 7 day windows
+7DAY_POSTS AS (
+    SELECT user_id,
+           post_date,
+           SUM(daily_posts) OVER(PARTITION BY user_id ORDER BY post_date ASC RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS 7day_posts
+    FROM AGG_DAILY
 ),
-D AS(
-    SELECT user_id, MAX(cnt7) AS max_7day_posts FROM C GROUP BY 1
-),
-E AS(
-    SELECT * FROM D INNER JOIN B USING(user_id) HAVING(avg_weekly_posts *2 <= max_7day_posts ) ORDER BY user_id
+-- get max 7day posts of each user
+MAX_7DAY_POSTS AS (
+    SELECT user_id,
+           MAX(7day_posts) AS max_7day_posts
+    FROM 7DAY_POSTS
+    GROUP BY user_id
 )
-SELECT * FROM E
+-- join avg_weekly with max_7day and filter
+SELECT M.user_id,
+       M.max_7day_posts,
+       A.avg_weekly_posts
+FROM MAX_7DAY_POSTS M
+INNER JOIN AVG_WEEKLY A
+ON M.user_id = A.user_id
+WHERE M.max_7day_posts >= 2 * A.avg_weekly_posts
+ORDER BY M.user_id ASC
