0f96d3162ca7c0f31082498336a985df4f5dd3f0
Queenie Chao <manling000713@gmail.com>
2024-05-30 17:09:24 -0700

    Time: 1522 ms (5.06%) | Memory: 0B (100.00%) - LeetSync

--- 2523-change-null-values-in-a-table-to-the-previous-value/change-null-values-in-a-table-to-the-previous-value.sql
+++ 2523-change-null-values-in-a-table-to-the-previous-value/change-null-values-in-a-table-to-the-previous-value.sql
@@ -1,23 +1,11 @@
 -- # Write your MySQL query statement below
--- WITH number AS(
---     SELECT id, drink, ROW_NUMBER() OVER() AS num
---     FROM CoffeeShop
--- ), group_num AS(
---     SELECT *, SUM(IF(drink IS NULL,0 , 1)) OVER(ORDER BY num) AS group_id
---     FROM number
--- )
-
--- SELECT id,  FIRST_VALUE(drink) OVER(PARTITION BY group_id) AS drink
--- FROM group_num
-with cte as (
-    select id, drink, row_number() over() as rn
-    from CoffeeShop
-),
-
-cte2 as (
-    select id, drink, rn, sum(if(drink is NULL, 0, 1)) over (order by rn) as group_id
-    from cte
+WITH number AS(
+    SELECT id, drink, ROW_NUMBER() OVER() AS num
+    FROM CoffeeShop
+), group_num AS(
+    SELECT *, SUM(IF(drink IS NULL, 0, 1)) OVER(ORDER BY num) AS group_id
+    FROM number
 )
 
-select id, first_value(drink) over (partition by group_id order by rn) as drink
-from cte2
+SELECT id,  FIRST_VALUE(drink) OVER(PARTITION BY group_id ORDER BY num) AS drink
+FROM group_num
