98668755acb53d73255b533dc12c59a9e4adaca0
Queenie Chao <manling000713@gmail.com>
2024-05-30 17:22:33 -0700

    Time: 2514 ms (12.59%) | Memory: 0B (100.00%) - LeetSync

--- 2534-employees-with-deductions/employees-with-deductions.sql
+++ 2534-employees-with-deductions/employees-with-deductions.sql
@@ -1,8 +1,14 @@
 # Write your MySQL query statement below
-SELECT e.employee_id
-FROM Employees e
-LEFT JOIN Logs l
-ON e.employee_id = l.employee_id
-GROUP BY e.employee_id, needed_hours
-HAVING SUM(CEIL(IFNULL(TIMESTAMPDIFF(SECOND, in_time, out_time), 0) / 60) / 60) < needed_hours
- 
+WITH session AS(
+    SELECT employee_id, 
+        SUM(CEIL(TIMESTAMPDIFF(SECOND, in_time, out_time)/60)) AS total_hours
+    FROM Logs
+    GROUP BY employee_id
+)
+SELECT  employee_id
+FROM Employees e 
+LEFT JOIN session s
+USING(employee_id)
+WHERE total_hours < needed_hours*60 OR total_hours IS NULL
+
+
