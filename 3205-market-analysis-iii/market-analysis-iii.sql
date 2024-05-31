b5a2f9661399fd871d2748b844fd3eef2ded622a
Queenie Chao <manling000713@gmail.com>
2024-05-30 17:53:00 -0700

    Time: 1276 ms (70.85%) | Memory: 0B (100.00%) - LeetSync

--- 3205-market-analysis-iii/market-analysis-iii.sql
+++ 3205-market-analysis-iii/market-analysis-iii.sql
@@ -1,18 +1,17 @@
 # Write your MySQL query statement below
-WITH cte AS(
-SELECT seller_id,
-       COUNT(DISTINCT item_id) as num_items 
-FROM Users 
-     JOIN Orders USING(seller_id)
-     JOIN Items USING(item_id)   
-WHERE favorite_brand<> item_brand
-GROUP BY seller_id
+WITH num AS(
+    SELECT seller_id, COUNT(DISTINCT item_id) AS num_items
+    FROM Orders
+    LEFT JOIN Users USING(seller_id)
+    LEFT JOIN Items USING(item_id)
+    WHERE favorite_brand <> item_brand
+    GROUP BY seller_id
 )
+
 SELECT seller_id, num_items
-FROM(
-    SELECT *, DENSE_RANK() OVER (ORDER BY num_items DESC) AS rk
-    FROM cte
-) AS sub
+FROM (
+    SELECT *, DENSE_RANK() OVER(ORDER BY num_items DESC) AS rk
+    FROM num
+) sub
 WHERE rk = 1
 ORDER BY seller_id
-
