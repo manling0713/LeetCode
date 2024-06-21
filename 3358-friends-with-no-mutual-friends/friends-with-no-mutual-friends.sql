d4ffce1c7e5dd6e3cf44c31d3049cb90ba24de16
Queenie Chao <manling000713@gmail.com>
2024-06-20 17:40:32 -0700

    Time: 432 ms (90.72%) | Memory: 0B (100.00%) - LeetSync

--- 3358-friends-with-no-mutual-friends/friends-with-no-mutual-friends.sql
+++ 3358-friends-with-no-mutual-friends/friends-with-no-mutual-friends.sql
@@ -1,24 +1,24 @@
--- Create a table with all friend pairs
-WITH all_pairs AS(
+# Write your MySQL query statement below
+WITH all_friend AS(
     SELECT user_id1 AS user_id
-        ,user_id2 AS friend_id
+        , user_id2 AS friend
     FROM Friends
     UNION ALL
     SELECT user_id2 AS user_id
-        ,user_id1 AS friend_id
+        , user_id1 AS friend
     FROM Friends
-),
-shared_friend_pairs AS(
-    SELECT a1.user_id AS user_id1
+), mutual AS(
+    SELECT a1.user_id As user_id1
         , a2.user_id AS user_id2
-    FROM all_pairs a1
-    LEFT JOIN all_pairs a2
-    USING(friend_id)
+    FROM all_friend a1
+    LEFT JOIN all_friend a2
+    USING(friend)
 )
-SELECT user_id1, user_id2
+
+SELECT *
 FROM Friends
-WHERE (user_id1, user_id2) NOT IN (
+WHERE(user_id1, user_id2) NOT IN(
     SELECT user_id1, user_id2
-    FROM shared_friend_pairs
+    FROM mutual
 )
 ORDER BY user_id1, user_id2
