d25b46011a856c4a4f0d4d5656ea3626b5ea0d64
Queenie Chao <manling000713@gmail.com>
2024-06-24 22:09:55 -0700

    Time: 368 ms (71.75%) | Memory: 0B (100.00%) - LeetSync

--- 3400-find-bursty-behavior/find-bursty-behavior.sql
+++ 3400-find-bursty-behavior/find-bursty-behavior.sql
@@ -4,7 +4,7 @@ WITH avg_post AS(
         , COUNT(post_id) OVER(PARTITION BY user_id)/4 AS avg_weekly_posts
         , COUNT(post_id) OVER(PARTITION BY user_id ORDER BY post_date RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS weekly_post
     FROM  Posts
-    WHERE post_date between '2024-02-01' AND '2024-02-28'
+    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
 ), max_post AS(
     SELECT user_id
         , MAX(weekly_post) OVER(PARTITIOn BY user_id) AS max_7day_posts
