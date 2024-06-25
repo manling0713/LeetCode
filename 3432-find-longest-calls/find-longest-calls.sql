a69793552f32e90689e9222a2aac79ae25a008bd
Queenie Chao <manling000713@gmail.com>
2024-06-24 22:23:09 -0700

    Time: 484 ms (92.99%) | Memory: 0B (100.00%) - LeetSync

--- 3432-find-longest-calls/find-longest-calls.sql
+++ 3432-find-longest-calls/find-longest-calls.sql
@@ -4,11 +4,11 @@ SELECT first_name
     , DATE_FORMAT(SEC_TO_TIME(duration), '%H:%i:%s') AS duration_formatted
 FROM(
     SELECT *
-        , ROW_NUMBER() OVER(PARTITION BY type ORDER BY duration DESC) AS num
-    FROM Calls ca
+        , ROW_NUMBER() OVER(PARTITION BY type ORDER BY duration DESC) AS rnk
+    FROM calls ca
     LEFT JOIN Contacts co
     ON ca.contact_id = co.id
-) comb
-WHERE num <= 3
+) sub
+WHERE rnk <= 3
 ORDER BY type, duration DESC
 
