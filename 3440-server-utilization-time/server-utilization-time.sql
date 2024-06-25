e2da6db96404b829524f80b76dfc15ca38bfc829
Queenie Chao <manling000713@gmail.com>
2024-06-25 15:56:48 -0700

    Time: 365 ms (96.20%) | Memory: 0B (100.00%) - LeetSync

--- 3440-server-utilization-time/server-utilization-time.sql
+++ 3440-server-utilization-time/server-utilization-time.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-SELECT FLOOR(SUM(TIMESTAMPDIFF(SECOND, status_start_time, status_end_time) )/(24 * 60 * 60)) AS total_uptime_days
-FROM(
+WITH start_end AS(
     SELECT server_id
         , session_status
         , CASE WHEN session_status = 'start' THEN status_time END AS status_start_time
         , LEAD(status_time) OVER(PARTITION BY server_id ORDER BY status_time) AS status_end_time
     FROM Servers
-) sub
+)
+SELECT FLOOR(SUM(TIMESTAMPDIFF(SECOND, status_start_time, status_end_time)/(24*60*60))) As total_uptime_days
+FROM start_end
 WHERE session_status = 'start'
