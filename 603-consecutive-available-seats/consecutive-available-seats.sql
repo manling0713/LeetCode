9f0dfcbbbb1319d3afc82e9df0435ff99c6ed4ba
Queenie Chao <manling000713@gmail.com>
2024-07-15 16:00:49 -0700

    Time: 338 ms (84.26%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,8 +1,5 @@
 # Write your MySQL query statement below
-SELECT seat_id
-FROM(
-    SELECT seat_id, free, LAG(free) OVER(ORDER BY seat_id) AS pre, LEAD(free) OVER(ORDER BY seat_id) AS next
-    FROM Cinema
-) AS st
-WHERE (free = 1 AND next=1) OR (free=1 AND pre=1)
-ORDER BY seat_id
+SELECT DISTINCT b.seat_id
+FROM Cinema a, Cinema b
+  WHERE abs(a.seat_id - b.seat_id) = 1
+  AND a.free = true AND b.free = true
