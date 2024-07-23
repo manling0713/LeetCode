67cd4ea1c56c265f9a5a0fa13b727ff17e78e54c
Queenie Chao <manling000713@gmail.com>
2024-07-22 23:32:51 -0700

    Time: 799 ms (57.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1162-project-employees-ii/project-employees-ii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT project_id
+FROM Project
+GROUP BY project_id
+HAVING COUNT(*) = (
+    SELECT COUNT(*)
+    FROM Project
+    GROUP BY project_id
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
+)
