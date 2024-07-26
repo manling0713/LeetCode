5b6ad01dc93db886fc499780d68c418e82ae2b27
Queenie Chao <manling000713@gmail.com>
2024-07-26 15:48:47 -0700

    Time: 915 ms (70.59%) | Memory: 0B (100.00%) - LeetSync

--- 1237-reported-posts-ii/reported-posts-ii.sql
+++ 1237-reported-posts-ii/reported-posts-ii.sql
@@ -1,11 +1,21 @@
 # Write your MySQL query statement below
-SELECT ROUND(AVG(cnt), 2) AS average_daily_percent
-FROM(
-    SELECT (COUNT(DISTINCT r.post_id))/COUNT(DISTINCT a.post_id) * 100 AS cnt
-    FROM Actions a 
+WITH report AS(
+    SELECT action_date, COUNT(DISTINCT post_id) total_spam
+    FROM Actions a
     LEFT JOIN Removals r
     USING(post_id)
-    WHERE extra = 'spam'
+    WHERE action = 'report' AND extra = 'spam'
     GROUP BY action_date
-) AS cnt
+), remove AS(
+    SELECT action_date, COUNT(DISTINCT post_id) total_remove
+    FROM Actions a
+    LEFT JOIN Removals r
+    USING(post_id)
+    WHERE action = 'report' AND extra = 'spam' AND remove_date IS NOT NULL
+    GROUP BY action_date
+)
 
+SELECT ROUND(AVG(IFNULL(total_remove, 0)/total_spam)*100, 2) AS average_daily_percent 
+FROM report
+LEFT JOIN remove
+USING(action_date)
