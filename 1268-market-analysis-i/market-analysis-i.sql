eaefecd4bb8d89ebba69874c01539d0538ee010f
Queenie Chao <manling000713@gmail.com>
2024-07-26 16:12:52 -0700

    Time: 1396 ms (32.25%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,14 +1,8 @@
 # Write your MySQL query statement below
-WITH order_2019 AS(
-    SELECT buyer_id
-        , COUNT(order_id) AS orders_in_2019
-    FROM Orders
-    WHERE year(order_date)=2019
-    GROUP BY buyer_id
-)
 SELECT user_id AS buyer_id
-    , join_date 
-    , IFNULL(orders_in_2019, 0) AS orders_in_2019
+    , join_date
+    , IFNULL(COUNT(order_date), 0) AS orders_in_2019
 FROM Users u
-LEFT JOIN order_2019 o
-ON u.user_id = o.buyer_id
+LEFT JOIN Orders o
+ON o.buyer_id = u.user_id AND YEAR(order_date) = 2019
+GROUP BY user_id
