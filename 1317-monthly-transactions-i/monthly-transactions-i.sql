6d7a35d6f102363fcef64d10f8e02ae39286eb73
Queenie Chao <manling000713@gmail.com>
2024-07-29 16:34:55 -0700

    Time: 611 ms (46.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT LEFT(trans_date, 7) AS month
+    , country
+    , COUNT(id) AS trans_count
+    , SUM(IF(state='approved',1 ,0)) AS approved_count
+    , SUM(amount) AS trans_total_amount
+    , SUM(IF(state='approved',amount ,0)) AS approved_total_amount
+FROM Transactions
+GROUP BY  YEAR(trans_date), MONTH(trans_date), country
