055aa0d4d63fd1b2ac51c3aa3e509580bccfbcce
Queenie Chao <manling000713@gmail.com>
2024-07-29 20:13:44 -0700

    Time: 719 ms (59.77%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -1,21 +1,22 @@
 # Write your MySQL query statement below
-WITH score AS(
-    SELECT host_team AS team_id,
-        CASE WHEN host_goals > guest_goals THEN 3
-        WHEN host_goals = guest_goals THEN 1
-        ELSE 0 END AS scores
-    FROM Matches
+WITH all_points AS(
+    SELECT host_team AS team_id
+        , CASE WHEN host_goals > guest_goals THEN 3
+            WHEN host_goals = guest_goals THEN 1
+            ELSE 0 END AS points
+    FROM Matches 
     UNION ALL
-    SELECT guest_team AS team_id,
-        CASE WHEN host_goals < guest_goals THEN 3
-        WHEN host_goals = guest_goals THEN 1
-        ELSE 0 END AS scores
-    FROM Matches
+    SELECT guest_team AS team_id
+        , CASE WHEN host_goals < guest_goals THEN 3
+            WHEN host_goals = guest_goals THEN 1
+            ELSE 0 END AS points
+    FROM Matches 
 )
-SELECT t.team_id, t.team_name, IFNULL(SUM(scores), 0) AS num_points
+SELECT t.team_id
+    , team_name
+    , IFNULL(SUM(points), 0) AS num_points
 FROM Teams t
-LEFT JOIN score s
+LEFT JOIN all_points a 
 USING(team_id)
 GROUP BY t.team_id
-ORDER BY num_points DESC, team_id
-
+ORDER BY num_points DESC, t.team_id
