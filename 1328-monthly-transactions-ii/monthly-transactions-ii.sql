601586ed99a4045f9303c070561bfd24a677ec9b
Queenie Chao <manling000713@gmail.com>
2024-07-29 19:52:45 -0700

    Time: 618 ms (60.48%) | Memory: 0B (100.00%) - LeetSync

--- 1328-monthly-transactions-ii/monthly-transactions-ii.sql
+++ 1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -1,12 +1,25 @@
-SELECT month, country, SUM(CASE WHEN state = "approved" THEN 1 ELSE 0 END) AS approved_count, SUM(CASE WHEN state = "approved" THEN amount ELSE 0 END) AS approved_amount, SUM(CASE WHEN state = "back" THEN 1 ELSE 0 END) AS chargeback_count, SUM(CASE WHEN state = "back" THEN amount ELSE 0 END) AS chargeback_amount
-FROM
-(
-    SELECT LEFT(chargebacks.trans_date, 7) AS month, country, "back" AS state, amount
-    FROM chargebacks
-    JOIN transactions ON chargebacks.trans_id = transactions.id
+WITH all_trans AS(
+    SELECT LEFT(trans_date, 7) AS month
+        , country
+        , state
+        , amount
+    FROM Transactions
+    WHERE state = 'approved'
     UNION ALL
-    SELECT LEFT(trans_date, 7) AS month, country, state, amount
-    FROM transactions
-    WHERE state = "approved"
-) s
+    SELECT LEFT(c.trans_date, 7) AS month
+        , country
+        , 'back' AS state
+        , amount
+    FROM Chargebacks c
+    LEFT JOIN Transactions t
+    ON c.trans_id = t.id
+)
+
+SELECT month
+    , country
+    , SUM(IF(state = 'approved', 1, 0)) AS approved_count
+    , SUM(IF(state = 'approved', amount, 0)) AS approved_amount
+    , SUM(IF(state = 'back', 1, 0)) AS chargeback_count
+    , SUM(IF(state = 'back', amount, 0)) AS chargeback_amount
+FROM all_trans
 GROUP BY month, country
