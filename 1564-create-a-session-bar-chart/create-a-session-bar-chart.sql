a18022d7793cde4dafedc2ae68957757a2f6530a
Queenie Chao <manling000713@gmail.com>
2024-08-13 16:32:43 -0700

    Time: 462 ms (25.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT '[0-5>' AS bin
+    , SUM(IF(duration < 300, 1, 0)) AS total
+FROM Sessions
+UNION ALL
+SELECT '[5-10>' AS bin
+    , SUM(IF(duration >= 300 AND duration < 600, 1, 0)) AS total
+FROM Sessions
+UNION ALL
+SELECT '[10-15>' AS bin
+    , SUM(IF(duration >= 600 AND duration < 900, 1, 0)) AS total
+FROM Sessions
+UNION ALL
+SELECT '15 or more' AS bin
+    , SUM(IF(duration >= 900, 1, 0)) AS total
+FROM Sessions
