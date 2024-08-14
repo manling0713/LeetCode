89b1de50570aaa2b064a218322b00c71cf912866
Queenie Chao <manling000713@gmail.com>
2024-08-14 15:04:58 -0700

    Time: 1369 ms (57.61%) | Memory: 0B (100.00%) - LeetSync

--- 1579-active-users/active-users.sql
+++ 1579-active-users/active-users.sql
@@ -1,8 +1,15 @@
+WITH id_count AS(
+    SELECT DISTINCT l1.id
+    FROM Logins l1
+    INNER JOIN Logins l2
+    ON l1.id = l2.id AND DATEDIFF(l1.login_date, l2.login_date) BETWEEN 1 AND 4
+    GROUP BY l1.id, l1.login_date
+    HAVING COUNT(DISTINCT l2.login_date) = 4
+)
 SELECT *
 FROM Accounts
-WHERE id IN
-    (SELECT DISTINCT t1.id 
-     FROM Logins t1 INNER JOIN Logins t2 on t1.id = t2.id AND DATEDIFF(t1.login_date, t2.login_date) BETWEEN 1 AND 4
-     GROUP BY t1.id, t1.login_date
-     HAVING COUNT(DISTINCT(t2.login_date)) = 4)
+WHERE id IN(
+    SELECT *
+    FROM id_count
+)
 ORDER BY id
