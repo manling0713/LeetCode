9495f7cdb3aea2eea5b0cd33993394425d7bffb5
Queenie Chao <manling000713@gmail.com>
2024-09-16 20:33:35 -0700

    Time: 535 ms (68.53%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,13 +1,20 @@
 # Write your MySQL query statement below
-WITH ppl AS(
-    SELECT from_id AS person1, to_id AS person2, duration
-    FROM Calls c1
-    WHERE from_id < to_id
-    UNION ALL 
-    SELECT to_id AS person1, from_id AS person2, duration
-    FROM Calls c1
-    WHERE from_id > to_id
+WITH all_call AS(
+    SELECT from_id AS person1
+        , to_id AS person2
+        , duration
+    FROM Calls
+    UNION ALL
+    SELECT to_id AS person1
+        , from_id AS person2
+        , duration
+    FROM Calls
 )
-SELECT person1, person2, COUNT(*) AS call_count, SUM(duration) AS total_duration
-FROM ppl
+SELECT person1
+    , person2
+    , COUNT(*) AS call_count
+    , SUM(duration) AS total_duration
+FROM all_call
+WHERE person1 < person2
 GROUP BY person1, person2
+
