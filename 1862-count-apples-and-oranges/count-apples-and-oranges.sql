4196d918bfaa50e40fc1bfb2042b7a252436eaaa
Queenie Chao <manling000713@gmail.com>
2024-09-17 16:17:36 -0700

    Time: 895 ms (73.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1862-count-apples-and-oranges/count-apples-and-oranges.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT SUM(b.apple_count + IFNULL(c.apple_count, 0)) AS apple_count
+    , SUM(b.orange_count + IFNULL(c.orange_count, 0)) AS orange_count
+FROM Boxes b
+LEFT JOIN Chests c
+USING(chest_id)
