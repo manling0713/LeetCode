f8f7ff2a7ac25ec225b4a89657a655bf9e487f84
Queenie Chao <manling000713@gmail.com>
2024-10-17 22:02:50 -0700

    Time: 716 ms (24.13%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
+
 SELECT s.user_id
-    , ROUND(AVG(IF(action='confirmed', 1, 0)),2) AS confirmation_rate
+    , ROUND(SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(*), 2) AS confirmation_rate
 FROM Signups s
-LEFT JOIN Confirmations c USING(user_id)
-GROUP BY s.user_id
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY user_id
