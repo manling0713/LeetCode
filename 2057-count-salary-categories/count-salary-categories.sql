934cb86b2cbcb70a9e2be383bc5633d4d5fd9973
Queenie Chao <manling000713@gmail.com>
2024-10-17 21:55:27 -0700

    Time: 1648 ms (56.41%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
-SELECT "Low Salary" AS category, COUNT(*) AS accounts_count
+
+SELECT 
+    "Low Salary" AS Category
+    , SUM(income < 20000) AS accounts_count
 FROM Accounts
-WHERE income < 20000
-UNION ALL
-SELECT "Average Salary" AS category, COUNT(*) AS accounts_count
+UNION
+SELECT
+    "Average Salary" Category
+    , SUM(income >= 20000 AND income <= 50000) AS accounts_count
 FROM Accounts
-WHERE income >= 20000 AND income <= 50000
-UNION ALL
-SELECT "High Salary" AS category, COUNT(*) AS accounts_count
+UNION
+SELECT
+    "High Salary" category
+    , SUM(income > 50000) AS accounts_count
 FROM Accounts
-WHERE income > 50000
