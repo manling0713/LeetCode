e67b5c84ecf67a3277815ae423969d22fd0719be
Queenie Chao <manling000713@gmail.com>
2024-10-24 16:07:47 -0700

    Time: 547 ms (48.91%) | Memory: 0B (100.00%) - LeetSync

--- 2143-count-the-number-of-experiments/count-the-number-of-experiments.sql
+++ 2143-count-the-number-of-experiments/count-the-number-of-experiments.sql
@@ -1,20 +1,22 @@
-# Write your MySQL query statement below
-WITH platforms AS (
+WITH all_platform AS (
     SELECT 'Android' AS platform
-    UNION ALL
+    UNION ALL 
     SELECT 'IOS' AS platform
-    UNION ALL
+    UNION ALL 
     SELECT 'Web' AS platform
-), experiment AS (
+), all_experiment AS(
     SELECT 'Reading' AS experiment_name
-    UNION ALL
+    UNION ALL 
     SELECT 'Sports' AS experiment_name
-    UNION ALL
+    UNION ALL 
     SELECT 'Programming' AS experiment_name
 )
-SELECT p.platform, ex.experiment_name, COUNT(e.experiment_name) AS num_experiments 
-FROM platforms p
-CROSS JOIN experiment ex
+
+SELECT p.platform
+    , a.experiment_name
+    , COUNT(e.experiment_id) AS num_experiments
+FROM all_platform AS p
+CROSS JOIN all_experiment AS a
 LEFT JOIN Experiments e
-ON p.platform = e.platform AND ex.experiment_name = e.experiment_name
-GROUP BY p.platform, ex.experiment_name
+    ON e.experiment_name = a.experiment_name AND p.platform = e.platform
+GROUP BY p.platform, a.experiment_name;
