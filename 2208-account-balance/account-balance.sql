9546982bef5cd393a450c393d5673a7bdfe99373
Queenie Chao <manling000713@gmail.com>
2024-10-25 15:51:30 -0700

    Time: 442 ms (66.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2208-account-balance/account-balance.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT account_id 
+    , day
+    , SUM(CASE WHEN type = 'Deposit' THEN amount ELSE -amount END) OVER(PARTITION BY account_id ORDER BY day) AS balance
+FROM Transactions
+ORDER BY account_id, day;
