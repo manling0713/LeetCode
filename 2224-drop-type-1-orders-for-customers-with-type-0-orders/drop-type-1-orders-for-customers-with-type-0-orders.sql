5535a01a9db026dfc95cd81ef7ea0fe44cfcfcc8
Queenie Chao <manling000713@gmail.com>
2024-10-28 16:31:06 -0700

    Time: 506 ms (29.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2224-drop-type-1-orders-for-customers-with-type-0-orders/drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT * 
+FROM Orders
+WHERE (customer_id, order_type) 
+IN (SELECT customer_id, MIN(order_type) 
+    FROM Orders 
+    GROUP BY customer_id)
