d8b019f8628b4575eeb7e7ebd4892c17553ce948
Queenie Chao <manling000713@gmail.com>
2024-10-29 21:07:55 -0700

    Time: 1250 ms (38.09%) | Memory: 0B (100.00%) - LeetSync

--- 2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
+++ 2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
@@ -1,14 +1,22 @@
 # Write your MySQL query statement below
-WITH bus_order AS(
-    SELECT bus_id, passenger_id, p.arrival_time, RANK() OVER(PARTITION BY passenger_id ORDER BY b.arrival_time) AS num
-    FROM Buses b, Passengers p
-    WHERE p.arrival_time <= b.arrival_time
+-- join 2 tables using p.arrival_time <= b.arrival_time
+-- WHERE b.arrival_time = MIN(b.arrival_time) group by passenger_id
+-- count(passenger_id) group by bus_id
+
+WITH bus_pass AS(
+    SELECT passenger_id
+        , MIN(b.arrival_time) AS bus_time
+    FROM Buses b
+    LEFT JOIN Passengers p
+    ON p.arrival_time <= b.arrival_time
+    GROUP BY passenger_id
 )
+
 SELECT b.bus_id
-    , SUM(IF(num=1,1,0)) AS passengers_cnt
+    , COUNT(bp.passenger_id) AS passengers_cnt
 FROM Buses b
-LEFT JOIN bus_order bo
-ON b.bus_id = bo.bus_id
+LEFT JOIN bus_pass bp
+ON bp.bus_time = b.arrival_time
 GROUP BY b.bus_id
-ORDER BY b.bus_id
+ORDER BY b.bus_id;
 
