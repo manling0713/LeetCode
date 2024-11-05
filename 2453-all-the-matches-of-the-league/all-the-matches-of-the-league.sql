0747ece863563fbdfe27af3d4f586147509505fc
Queenie Chao <manling000713@gmail.com>
2024-11-05 14:36:07 -0800

    Time: 721 ms (22.00%) | Memory: 0B (100.00%) - LeetSync

--- 2453-all-the-matches-of-the-league/all-the-matches-of-the-league.sql
+++ 2453-all-the-matches-of-the-league/all-the-matches-of-the-league.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT t1.team_name AS home_team, t2.team_name AS away_team
+SELECT  t1.team_name AS home_team
+    , t2.team_name AS away_team
 FROM Teams t1, Teams t2
 WHERE t1.team_name <> t2.team_name
