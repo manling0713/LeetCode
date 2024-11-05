33b08bc3dc44e74d4464e5713ab68bb00b9f2e7e
Queenie Chao <manling000713@gmail.com>
2024-11-05 14:31:25 -0800

    Time: 993 ms (48.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2452-product-sales-analysis-v/product-sales-analysis-v.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT user_id
+    , SUM(quantity * price) AS spending
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY user_id
+ORDER BY spending DESC, user_id
