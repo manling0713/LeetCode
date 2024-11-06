c8abccba15dd08780dcce32d3bbeafa71a4d8611
Queenie Chao <manling000713@gmail.com>
2024-11-06 14:12:39 -0800

    Time: 846 ms (71.19%) | Memory: 0B (100.00%) - LeetSync

--- 2484-compute-the-rank-as-a-percentage/compute-the-rank-as-a-percentage.sql
+++ 2484-compute-the-rank-as-a-percentage/compute-the-rank-as-a-percentage.sql
@@ -1,14 +1,19 @@
 # Write your MySQL query statement below
-WITH cte1 AS(
-    SELECT *, rank() OVER(PARTITION BY department_id ORDER BY mark DESC) AS student_rank_in_the_department
+WITH student_rank AS(
+    SELECT student_id
+        , department_id
+        , RANK() OVER(PARTITION BY department_id ORDER BY mark DESC) AS student_rank_in_the_department 
     FROM Students
-), cte2 AS(
-    SELECT department_id, COUNT(*) AS the_number_of_students_in_the_department
+), student_cnt AS(
+    SELECT department_id
+        , COUNT(student_id) AS the_number_of_students_in_the_department
     FROM Students
     GROUP BY department_id
 )
 
-SELECT student_id, cte1.department_id,
-    ROUND(IFNULL((student_rank_in_the_department - 1) * 100 / (IFNULL(the_number_of_students_in_the_department, 0) - 1) ,0), 2) AS percentage
-FROM cte1
-LEFT JOIN cte2 USING(department_id)
+SELECT student_id
+    , r.department_id
+    , ROUND(IFNULL((student_rank_in_the_department - 1) * 100 / (the_number_of_students_in_the_department - 1), 0), 2) AS percentage
+FROM student_rank r
+LEFT JOIN student_cnt c
+ON r.department_id = c.department_id
