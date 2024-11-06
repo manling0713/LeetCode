56e7d3cc7c5af7714963d09c2dc2306d52bfffa1
Queenie Chao <manling000713@gmail.com>
2024-11-06 14:29:26 -0800

    Time: 1218 ms (57.41%) | Memory: 0B (100.00%) - LeetSync

--- 2515-calculate-the-influence-of-each-salesperson/calculate-the-influence-of-each-salesperson.sql
+++ 2515-calculate-the-influence-of-each-salesperson/calculate-the-influence-of-each-salesperson.sql
@@ -1,14 +1,10 @@
 # Write your MySQL query statement below
-WITH cte AS (
-    SELECT sp.salesperson_id, SUM(price) AS total
-    FROM Sales s
-    LEFT JOIN Customer c
-    ON s.customer_id = c.customer_id
-    LEFT JOIN Salesperson sp 
-    ON c.salesperson_id = sp.salesperson_id
-    GROUP BY sp.salesperson_id 
-)
-
-SELECT s.salesperson_id, name, IFNULL(total, 0) AS total
-FROM Salesperson s
-LEFT JOIN cte USING(salesperson_id)
+SELECT sp.salesperson_id
+    , name
+    , IFNULL(SUM(price), 0) AS total
+FROM Salesperson sp
+LEFT JOIN Customer c
+ON sp.salesperson_id = c.salesperson_id
+LEFT JOIN Sales s
+ON s.customer_id = c.customer_id
+GROUP BY sp.salesperson_id;
