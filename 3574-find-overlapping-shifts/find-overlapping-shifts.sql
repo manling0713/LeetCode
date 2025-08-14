9f37efce08fe2b6cdc6357b49b626a2aa026441d
Queenie Chao <manling000713@gmail.com>
2025-08-13 17:08:40 -0700

    Time: 274 ms (70.91%) | Memory: 0B (100.00%) - LeetSync

--- 3574-find-overlapping-shifts/find-overlapping-shifts.sql
+++ 3574-find-overlapping-shifts/find-overlapping-shifts.sql
@@ -1,23 +1,5 @@
 # Write your MySQL query statement below
 
--- WITH all_shifts AS(
---     SELECT employee_id
---         , start_time
---         , end_time
---         , LEAD(start_time, 1) OVER(PARTITION BY employee_id ORDER BY start_time) AS next_shift_start
---     FROM EmployeeShifts
--- ), overlap_shifts AS(
---     SELECT *
---     FROM all_shifts
---     WHERE next_shift_start BETWEEN start_time AND end_time
--- )
-
--- SELECT employee_id
---     , COUNT(*) AS overlapping_shifts
--- FROM overlap_shifts
--- GROUP BY employee_id
--- ORDER BY employee_id;
-
 WITH overlap_shift AS(
     SELECT e1.employee_id
         , e1.start_time
