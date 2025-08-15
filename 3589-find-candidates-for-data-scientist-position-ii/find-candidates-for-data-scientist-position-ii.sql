86d174518bd6fb3b4385f3983344e0ff7650295d
Queenie Chao <manling000713@gmail.com>
2025-08-14 22:30:39 -0700

    Time: 507 ms (60.16%) | Memory: 0B (100.00%) - LeetSync

--- 3589-find-candidates-for-data-scientist-position-ii/find-candidates-for-data-scientist-position-ii.sql
+++ 3589-find-candidates-for-data-scientist-position-ii/find-candidates-for-data-scientist-position-ii.sql
@@ -1,64 +1,37 @@
 # Write your MySQL query statement below
--- join table using skill 
--- LEFT JOIN Candidates
--- using case when to calculate the score
--- SUM score GROUP BY candidate_id
--- ROW_NUMBER() OVER(PARTITION BY project_id ORDER BY score DESC, candidate_id)
--- WHERE candidate_id is not null
-
--- WITH project_candidate AS(
---     SELECT project_id
---         , candidate_id
---         , p.skill
---         , CASE WHEN proficiency > importance THEN 10
---             WHEN proficiency < importance THEN -5
---             WHEN proficiency IS NULL THEN -1000
---             ELSE 0 END AS score
---     FROM Projects p
---     LEFT JOIN Candidates c
---     ON p.skill = c.skill
--- ), total_score AS(
---     SELECT project_id
---         , candidate_id
---         , SUM(score) + 100 AS sum_score
---         , ROW_NUMBER() OVER(PARTITION BY project_id ORDER BY SUM(score) DESC, candidate_id) AS rnk
---     FROM project_candidate
---     GROUP BY project_id, candidate_id
--- )
-
--- SELECT project_id
---     , candidate_id
---     , sum_score AS score
--- FROM total_score
--- WHERE rnk = 1
---     AND sum_score > 0
--- ORDER BY project_id;
-
-with candidate_info as(
-    select candidate_id,project_id,count(skill)skills,
-    100+sum(
-        case when proficiency>importance  then 10
-        when proficiency<importance  then -5
-        else 0 end
-    )score
-    from Candidates 
-    join projects
-    using(skill)
-    group by 1,2
-),
-project_info as(
-select project_id  ,count(skill)skills
-from projects
-group by 1
-),
-cte as(
-    select project_id  ,candidate_id ,score,
-    dense_rank() over(partition by project_id order by score desc,candidate_id )rnk
-    from candidate_info
-    join project_info
-    using(skills,project_id)
+WITH candidate_info AS (
+    SELECT candidate_id
+        , project_id
+        , COUNT(c.skill) AS can_skills
+        , SUM(
+            CASE WHEN proficiency > importance THEN 10
+            WHEN proficiency < importance THEN -5
+            ELSE 0 END 
+        ) + 100 AS score
+    FROM Candidates c
+    LEFT JOIN Projects p
+    ON c.skill = p.skill
+    GROUP BY candidate_id, project_id
+), project_info AS (
+    SELECT project_id 
+        , COUNT(skill) AS proj_skills
+    FROM Projects
+    GROUP BY project_id
+), candidate_rank AS(
+    SELECT candidate_id
+        , project_id
+        , score
+        , ROW_NUMBER() OVER(PARTITION BY project_id ORDER BY score DESC, candidate_id) AS rnk
+    FROM candidate_info
+    WHERE (project_id, can_skills) IN (
+        SELECT project_id, proj_skills
+        FROM project_info
     )
-    select project_id  ,candidate_id ,score
-    from cte
-    where rnk=1
+)
 
+SELECT project_id
+    , candidate_id
+    , score
+FROM candidate_rank
+WHERE rnk = 1
+ORDER BY project_id;
