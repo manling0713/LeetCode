c8fc31858e61eace6ddd26b77609cb12ee43da9e
Queenie Chao <manling000713@gmail.com>
2025-08-24 17:21:51 -0700

    Time: 537 ms (49.78%) | Memory: 0B (100.00%) - LeetSync

--- 3932-find-covid-recovery-patients/find-covid-recovery-patients.sql
+++ 3932-find-covid-recovery-patients/find-covid-recovery-patients.sql
@@ -1,15 +1,28 @@
 # Write your MySQL query statement below
--- WHERE result = 'Positive'
--- ROW_NUMBER() OVER(PARTITION BY patient_id, result ORDER BY test_date) rnk = 1
--- JOIN positive and negative
--- p.test_date < n.test_date
--- LEFT JOIN patients ON patient_id
--- n.test_date - p.test_date AS recovery_time
+SELECT c1.patient_id
+    , patient_name
+    , age
+    , DATEDIFF(MIN(c2.test_date), MIN(c1.test_date)) AS recovery_time
+FROM covid_tests c1
+INNER JOIN covid_tests c2
+ON c1.patient_id = c2.patient_id
+    AND c1.test_date < c2.test_date
+    AND c1.result = 'Positive' AND c2.result = 'Negative'
+LEFT JOIN patients p
+ON c1.patient_id = p.patient_id
+GROUP BY c1.patient_id, patient_name, age
+ORDER BY recovery_time, patient_name;
 
--- Write your MySQL query statement below
-
-SELECT c.patient_id, p.patient_name, p.age, datediff(MIN(c1.test_date) , MIN(c.test_date)) recovery_time
-FROM covid_tests c INNER JOIN covid_tests c1 ON c.patient_id = c1.patient_id AND c.test_date < c1.test_date AND c.result = 'Positive' AND c1.result = 'Negative'
-INNER JOIN patients p ON c.patient_id = p.patient_id
-GROUP BY c.patient_id, p.patient_name, p.age
-ORDER BY recovery_time, p.patient_name;
+-- SELECT c.patient_id
+--     , p.patient_name
+--     , p.age
+--     , datediff(MIN(c1.test_date) , MIN(c.test_date)) recovery_time
+-- FROM covid_tests c 
+-- INNER JOIN covid_tests c1 
+-- ON c.patient_id = c1.patient_id 
+--     AND c.test_date < c1.test_date 
+--     AND c.result = 'Positive' AND c1.result = 'Negative'
+-- INNER JOIN patients p 
+-- ON c.patient_id = p.patient_id
+-- GROUP BY c.patient_id, p.patient_name, p.age
+-- ORDER BY recovery_time, p.patient_name;
