f2cbc016a216eddaed8543537ca3fce8667e142b
Queenie Chao <manling000713@gmail.com>
2025-08-30 15:16:21 -0700

    Time: 361 ms (82.93%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-SELECT DISTINCT c1.seat_id
-FROM Cinema c1, Cinema c2
-  WHERE abs(c1.seat_id - c2.seat_id) = 1
-  AND c1.free = 1 AND c2.free = 1
-ORDER BY c1.seat_id
+-- SELECT c1.seat_id
+-- FROM Cinema c1
+-- LEFT JOIN Cinema c2
+-- ON c1.seat_id = c2.seat_id - 1
+-- WHERE c1.free = 1
+--     AND (c2.free = 1 OR c2.free IS NULL)
+-- ORDER BY 1;
+select distinct t1.seat_id from cinema as t1 
+inner join cinema as t2 
+on abs(t1.seat_id - t2.seat_id) = 1 and 
+t1.free =1 and t2.free =1 
+order by t1.seat_id ; 
