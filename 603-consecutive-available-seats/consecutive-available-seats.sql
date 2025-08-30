38667d7989a9a26a75ab5142614aa0bfbd518b4f
Queenie Chao <manling000713@gmail.com>
2025-08-30 15:20:54 -0700

    Time: 336 ms (98.88%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -6,10 +6,3 @@ ON ABS(c1.seat_id - c2.seat_id) = 1
 WHERE c1.free = 1
     AND c2.free = 1
 ORDER BY 1;
-
--- select distinct t1.seat_id 
--- from cinema as t1 
--- inner join cinema as t2 
--- on abs(t1.seat_id - t2.seat_id) = 1 and 
--- t1.free =1 and t2.free =1 
--- order by t1.seat_id ; 
