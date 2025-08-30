0fb66d37bef3934f3f9f2e6c5147a2d94fc05b35
Queenie Chao <manling000713@gmail.com>
2025-08-30 14:59:02 -0700

    Time: 366 ms (94.28%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT w1.id
+SELECT DISTINCT w1.id
 FROM Weather w1
 LEFT JOIN Weather w2
-ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
-WHERE w1.temperature > w2.temperature 
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
