d06002abd5348688c363f723171b0871777adf0d
Queenie Chao <manling000713@gmail.com>
2025-09-10 14:38:34 -0700

    Time: 1203 ms (62.43%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-SELECT user_id AS buyer_id
+SELECT u.user_id AS buyer_id
     , join_date
-    , IFNULL(COUNT(order_date), 0) AS orders_in_2019
-FROM Users u
-LEFT JOIN Orders o
-ON o.buyer_id = u.user_id AND YEAR(order_date) = 2019
-GROUP BY user_id
+    , SUM(CASE WHEN YEAR(order_date) = 2019 THEN 1 ELSE 0 END) AS orders_in_2019
+FROM users u
+LEFT JOIN Orders l
+ON u.user_id = l.buyer_id
+GROUP BY 1, 2;
