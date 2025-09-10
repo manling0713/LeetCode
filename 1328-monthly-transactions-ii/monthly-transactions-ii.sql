73039ed2b073d4e784447d0d3d81214572ac5e6a
Queenie Chao <manling000713@gmail.com>
2025-09-10 15:00:45 -0700

    Time: 666 ms (49.70%) | Memory: 0B (100.00%) - LeetSync

--- 1328-monthly-transactions-ii/monthly-transactions-ii.sql
+++ 1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -1,25 +1,17 @@
-WITH all_trans AS(
-    SELECT LEFT(trans_date, 7) AS month
-        , country
-        , state
-        , amount
-    FROM Transactions
-    WHERE state = 'approved'
-    UNION ALL
-    SELECT LEFT(c.trans_date, 7) AS month
-        , country
-        , 'back' AS state
-        , amount
-    FROM Chargebacks c
-    LEFT JOIN Transactions t
-    ON c.trans_id = t.id
+# Write your MySQL query statement below
+with cte as(
+    select * from Transactions  where state='approved'
+    union all
+    select id,country,'cbe' state,amount,b.trans_date
+    from Chargebacks b
+    left join Transactions a
+on a.id=b.trans_id
 )
+select date_format(trans_date,'%Y-%m') month,country,
+sum(state='approved')approved_count,
+sum(if(state='approved',amount,0)) approved_amount ,
+sum(state='cbe') chargeback_count ,
+sum(if(state='cbe',amount,0))chargeback_amount 
+from cte
+group by 1,2
 
-SELECT month
-    , country
-    , SUM(IF(state = 'approved', 1, 0)) AS approved_count
-    , SUM(IF(state = 'approved', amount, 0)) AS approved_amount
-    , SUM(IF(state = 'back', 1, 0)) AS chargeback_count
-    , SUM(IF(state = 'back', amount, 0)) AS chargeback_amount
-FROM all_trans
-GROUP BY month, country
