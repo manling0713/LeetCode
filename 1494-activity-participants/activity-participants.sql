91dc10dfe22ef806d6abc72d2fcf3d9a5c4b3bff
Queenie Chao <manling000713@gmail.com>
2025-09-12 20:45:44 -0700

    Time: 609 ms (97.01%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-SELECT activity 
-FROM friends
-GROUP BY activity
-HAVING COUNT(*)> (SELECT COUNT(*) FROM friends GROUP BY activity ORDER BY 1 LIMIT 1)
-AND COUNT(*)< (SELECT COUNT(*) FROM friends GROUP BY activity ORDER BY 1 DESC LIMIT 1)
+WITH activity_rank AS(
+    SELECT activity
+        , RANK() OVER(ORDER BY COUNT(id) DESC) AS rnk1
+        , RANK() OVER(ORDER BY COUNT(id)) AS rnk2
+    FROM Friends
+    GROUP BY activity
+)
+SELECT activity
+FROM activity_rank
+WHERE rnk1 <> 1 AND rnk2 <> 1
+
