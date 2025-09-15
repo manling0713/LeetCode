f8a3adff794ee47037262141b407b3921bd1c364
Queenie Chao <manling000713@gmail.com>
2025-09-15 15:16:48 -0700

    Time: 548 ms (44.90%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -1,10 +1,17 @@
 # Write your MySQL query statement below
-WITH user_diff AS(
+WITH user_visit AS(
     SELECT user_id
-        , DATEDIFF(LEAD(visit_date, 1, '2021-1-1') OVER(PARTITION BY user_id ORDER BY visit_date), visit_date) AS diff
+        , visit_date
+        , IF(LEAD(visit_date, 1) OVER(PARTITION BY user_id ORDER BY visit_date) IS NOT NULL, LEAD(visit_date, 1) OVER(PARTITION BY user_id ORDER BY visit_date), '2021-1-1') AS next_visit
     FROM UserVisits
+), user_window AS(
+    SELECT user_id
+        , DATEDIFF(next_visit, visit_date) AS visit_window
+    FROM user_visit
 )
-SELECT user_id, MAX(diff) AS biggest_window
-FROM user_diff
+SELECT user_id
+    , MAX(visit_window) AS biggest_window
+FROM user_window
 GROUP BY user_id
 ORDER BY user_id
+
