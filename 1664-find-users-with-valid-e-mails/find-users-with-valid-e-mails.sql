08bdce7b99cb651c4b524f577c82c48055b30d42
Queenie Chao <manling000713@gmail.com>
2025-09-15 14:44:49 -0700

    Time: 677 ms (75.28%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Users
-WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$'
+  AND BINARY substring_index(mail, '@', -1) = 'leetcode.com';
