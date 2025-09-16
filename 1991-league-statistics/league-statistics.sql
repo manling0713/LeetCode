46d52d752cf8e42fdba371bde3b3682e11293d74
Queenie Chao <manling000713@gmail.com>
2025-09-16 14:45:17 -0700

    Time: 920 ms (94.55%) | Memory: 0B (100.00%) - LeetSync

--- 1991-league-statistics/league-statistics.sql
+++ 1991-league-statistics/league-statistics.sql
@@ -1,24 +1,24 @@
 # Write your MySQL query statement below
-WITH all_matches AS(
+WITH all_champ AS(
     SELECT home_team_id
-        , home_team_goals AS home
-        , away_team_goals AS away 
-    FROM matches
+        , home_team_goals
+        , away_team_goals
+    FROM Matches
     UNION ALL
     SELECT away_team_id AS home_team_id
-        , away_team_goals AS home
-        , home_team_goals AS away 
-    FROM matches
+        , away_team_goals AS home_team_goals
+        , home_team_goals AS away_team_goals
+    FROM Matches
 )
-
 SELECT team_name
     , COUNT(*) AS matches_played
-    , SUM(CASE WHEN home > away THEN 3
-            WHEN home = away THEN 1 ELSE 0 END) AS points
-    , SUM(home) AS goal_for
-    , SUM(away) AS goal_against
-    , SUM(home) - SUM(away) AS goal_diff
-FROM all_matches a
+    , SUM(CASE WHEN home_team_goals > away_team_goals THEN 3
+        WHEN home_team_goals = away_team_goals THEN 1
+        ELSE 0 END) AS points
+    , SUM(home_team_goals) AS goal_for
+    , SUM(away_team_goals) AS goal_against
+    , SUM(home_team_goals) - SUM(away_team_goals) AS goal_diff
+FROM all_champ a
 LEFT JOIN teams t
 ON a.home_team_id = t.team_id
 GROUP BY team_name
