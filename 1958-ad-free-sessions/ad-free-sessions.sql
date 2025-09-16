2316a1f0327ae525ec16e171564438fd6f1856a8
Queenie Chao <manling000713@gmail.com>
2025-09-16 14:26:48 -0700

    Time: 743 ms (96.79%) | Memory: 0B (100.00%) - LeetSync

--- 1958-ad-free-sessions/ad-free-sessions.sql
+++ 1958-ad-free-sessions/ad-free-sessions.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT DISTINCT session_id
-FROM Playback
-WHERE session_id NOT IN 
-(SELECT DISTINCT p.session_id
-FROM Playback p LEFT JOIN Ads a ON p.customer_id = a.customer_id
-WHERE a.timestamp BETWEEN p.start_time AND p.end_time)
+SELECT session_id
+FROM Playback p
+LEFT JOIN Ads a
+ON p.customer_id = a.customer_id
+    AND timestamp BETWEEN start_time AND end_time
+WHERE ad_id IS NULL
