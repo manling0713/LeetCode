3bef6a3cb1338aaa0ea9d85ca67c47849928f5c4
Queenie Chao <manling000713@gmail.com>
2025-09-18 15:54:30 -0700

    Time: 1083 ms (31.84%) | Memory: 0B (100.00%) - LeetSync

--- 1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
+++ 1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
@@ -1,26 +1,16 @@
 # Write your MySQL query statement below
--- account_id, SUM(amount)
--- group by account_id, month
--- SUM(amount) > max_income
-
-# Write your MySQL query statement below
-WITH income_table AS(
-    SELECT account_id
-        , DATE_FORMAT(day, '%Y%m') AS months
-        , sum(amount) as total_income 
-    FROM Transactions 
-    WHERE type = 'Creditor'
-    GROUP BY account_id, DATE_FORMAT(day, '%Y%m')
-), time_table AS(
-    SELECT i.account_id
-        , months,total_income
-        , LEAD(months, 1) OVER(PARTITION BY account_id ORDER BY months) as next_month  
-FROM income_table i 
-LEFT JOIN Accounts a
-ON i.account_id = a.account_id
-WHERE total_income > max_income
+with cte as(
+    select account_id ,date_format(day,'%y%m') tym,
+    sum(amount) amt,max_income 
+    from Accounts 
+    join Transactions 
+    using(account_id )
+    where type='Creditor'
+    group by 1,2
+    having amt>max_income
 )
-
-SELECT DISTINCT account_id
-FROM time_table 
-WHERE next_month - months = 1 
+select distinct account_id 
+from cte a
+join cte b
+using(account_id )
+where period_diff(a.tym,b.tym)=1
