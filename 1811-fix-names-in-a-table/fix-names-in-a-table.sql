c3421bf088083156247d26e7acb458824cb14371
Queenie Chao <manling000713@gmail.com>
2026-03-03 17:27:47 -0800

    Time: 588 ms (90.76%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, length(name)-1))) AS name
+SELECT user_id
+   , CONCAT(UPPER(SUBSTR(name, 1, 1)), LOWER(SUBSTR(name, 2))) AS name
 FROM Users
 ORDER BY user_id
