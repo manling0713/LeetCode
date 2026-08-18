7b9ac960c3486e46f609fb1c2595c92f602ddf4f
Queenie Chao <manling000713@gmail.com>
2026-08-18 02:37:17 -0700

    Time: 474 ms (8.38%) | Memory: 0B (100.00%) - LeetSync

--- 4091-find-golden-hour-customers/find-golden-hour-customers.sql
+++ 4091-find-golden-hour-customers/find-golden-hour-customers.sql
@@ -11,14 +11,3 @@ HAVING total_orders >= 3
     AND average_rating >= 4.0
     AND SUM(CASE WHEN order_rating IS NOT NULL THEN 1 ELSE 0 END)/ COUNT(order_id) >= 0.5
 ORDER BY average_rating DESC, customer_id DESC
-
-
--- select  
---     customer_id, 
---     count(customer_id) as total_orders, 
---     round(avg(case when ((time(order_timestamp) >= '11:00:00' and time(order_timestamp) <= '14:00:00') or (time(order_timestamp) >= '18:00:00' and time(order_timestamp) <= '21:00:00')) then 1 else 0 end) * 100, 0) as peak_hour_percentage,
---     round(avg(order_rating), 2) as average_rating
--- from restaurant_orders
--- group by customer_id
--- having total_orders > 2 and average_rating >= 4.0 and peak_hour_percentage >= 60 and round(avg(case when order_rating is not NULL then 1 else 0 end)*100, 0) >= 50
--- order by average_rating desc, customer_id desc
