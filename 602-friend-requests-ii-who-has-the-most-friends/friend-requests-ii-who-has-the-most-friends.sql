bc6b25265440435f8b28b10fca3f35e970196d11
Queenie Chao <manling000713@gmail.com>
2026-08-19 22:58:01 -0700

    Time: 378 ms (24.80%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,13 +1,16 @@
 # Write your MySQL query statement below
-WITH all_friend AS(
-    SELECT requester_id AS id, accepter_id AS friend
+WITH id_friend AS(
+    SELECT requester_id AS id
+        , accepter_id AS friend_id
     FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id AS id, requester_id AS friend
+    UNION
+    SELECT accepter_id AS id
+        , requester_id AS friend_id
     FROM RequestAccepted
 )
-SELECT id, COUNT(friend) AS num
-FROM all_friend
+SELECT id
+    , COUNT(DISTINCT friend_id) AS num
+FROM id_friend
 GROUP BY id
 ORDER BY num DESC
-LIMIT 1
+LIMIT 1;
