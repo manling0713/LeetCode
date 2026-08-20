6b7e547fc39aff220d22f4257b22dc151734532f
Queenie Chao <manling000713@gmail.com>
2026-08-19 22:46:32 -0700

    Time: 444 ms (17.99%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-WITH reports AS(
-    SELECT managerId
-    FROM Employee
-    GROUP BY managerId
-    HAVING COUNT(*) >= 5
-)
-SELECT name
-FROM Employee e  
-LEFT JOIN reports r
-ON e.id = r.managerId
-WHERE r.managerId IS NOT NULL
+-- SELECT DISTINCT e1.name
+-- FROM Employee e1
+-- LEFT JOIN Employee e2
+-- ON e1.id = e2.managerId
+-- GROUP BY e1.name
+-- HAVING COUNT(e2.id) >= 5
+
+select name from Employee 
+where id in (select managerId from Employee group by managerId 
+having count(managerId)>4);
